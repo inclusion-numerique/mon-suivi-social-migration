8a8a62eb82f9f289e2a504272a8ec366bb1a9f72
Mlbiche <tmlb@pm.me>
2023-04-20 14:24:41 +0200

    refactor: update .env to adapt to more configurations

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
-import { dump } from "./src/scripts/dump.mjs";
-import { migrate } from "./src/scripts/migrate.mjs";
-import { prepare } from "./src/scripts/prepare.mjs";
-import { restore } from "./src/scripts/restore.mjs";
+import { dump } from './src/scripts/dump.mjs';
+import { migrate } from './src/scripts/migrate.mjs';
+import { prepare } from './src/scripts/prepare.mjs';
+import { restore } from './src/scripts/restore.mjs';
 
 await dump();
 await prepare();
