8bce4832e2402472b87df0a4b28bee171011dcd3
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2023-07-05 15:55:37 +0200

    fix: migration

--- sql/16_user_activity/pre_migration.sql
+++ sql/16_user_activity/pre_migration.sql
@@ -2,22 +2,10 @@ delete from directus.directus_activity where "action" = 'run';
 delete from directus.directus_activity where "user" is null;
 delete from directus.directus_activity where "user" not in (select id from directus.directus_users);
 
-UPDATE directus.directus_activity set "action" = 'LOGIN' where "action" = 'login';
-UPDATE directus.directus_activity set "action" = 'DELETE' where "action" = 'delete';
-UPDATE directus.directus_activity set "action" = 'UPDATE' where "action" = 'update';
-UPDATE directus.directus_activity set "action" = 'CREATE' where "action" = 'create';
-
-UPDATE directus.directus_activity set "collection" = 'Followup' where "collection" = 'follow_ups';
-UPDATE directus.directus_activity set "collection" = 'User' where "collection" = 'directus_users';
-UPDATE directus.directus_activity set "collection" = 'HelpRequest' where "collection" = 'help_requests';
-UPDATE directus.directus_activity set "collection" = 'Beneficiary' where "collection" = 'beneficiary';
-UPDATE directus.directus_activity set "collection" = 'Structure' where "collection" = 'organisation';
-UPDATE directus.directus_activity set "collection" = 'Comment' where "collection" = 'comments';
-UPDATE directus.directus_activity set "collection" = 'Notification' where "collection" = 'notifications';
-UPDATE directus.directus_activity set "collection" = 'Document' where "collection" = 'directus_files';
-
 truncate directus.directus_revisions;
 
+delete from directus.directus_activity where "collection" = 'directus_roles';
+delete from directus.directus_activity where "collection" = 'directus_panels';
 delete from directus.directus_activity where "collection" = 'directus_operations';
 delete from directus.directus_activity where "collection" = 'organisation_follow_up_types';
 delete from directus.directus_activity where "collection" = 'directus_fields';
@@ -36,4 +24,18 @@ delete from directus.directus_activity where "collection" = 'organismes_instruct
 delete from directus.directus_activity where "collection" = 'directus_flows';
 delete from directus.directus_activity where "collection" = 'relatives';
 delete from directus.directus_activity where "collection" = 'organismes_prescripteurs';
- 
+
+UPDATE directus.directus_activity set "action" = 'LOGIN' where "action" = 'login';
+UPDATE directus.directus_activity set "action" = 'DELETE' where "action" = 'delete';
+UPDATE directus.directus_activity set "action" = 'UPDATE' where "action" = 'update';
+UPDATE directus.directus_activity set "action" = 'CREATE' where "action" = 'create';
+
+UPDATE directus.directus_activity set "collection" = 'Followup' where "collection" = 'follow_ups';
+UPDATE directus.directus_activity set "collection" = 'User' where "collection" = 'directus_users';
+UPDATE directus.directus_activity set "collection" = 'HelpRequest' where "collection" = 'help_requests';
+UPDATE directus.directus_activity set "collection" = 'Beneficiary' where "collection" = 'beneficiary';
+UPDATE directus.directus_activity set "collection" = 'Structure' where "collection" = 'organisation';
+UPDATE directus.directus_activity set "collection" = 'Comment' where "collection" = 'comments';
+UPDATE directus.directus_activity set "collection" = 'Notification' where "collection" = 'notifications';
+UPDATE directus.directus_activity set "collection" = 'Document' where "collection" = 'directus_files';
+
